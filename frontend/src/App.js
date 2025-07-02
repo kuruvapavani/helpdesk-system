09f4412e0a5d1490237c70e859bde34b2e532050
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-07-02 12:50:05 +0530

    project setup with tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
+      Tailwind is Working 🎉
+    </div>
+  );
+};
+
+export default App;
